2eeca0d23e9cd9ff2f26dfa5f2e8aaa63d8efcf9
oluwatobi <oluwatobiajayi001@gmail.com>
2024-07-03 11:43:24 +0100

    modified:   src/components/Ref.jsx

--- src/components/Ref.jsx
+++ src/components/Ref.jsx
@@ -1,9 +1,32 @@
+import { useEffect } from "react";
 import { useRef } from "react";
 
 export default function Ref() {
   const firstNameRef = useRef(null);
   const secondNameRef = useRef(null);
   const submitRef = useRef(null);
+
+  function firstKeyDown(e) {
+    if (e.key === "Enter") {
+      secondNameRef.current.focus();
+    }
+  }
+
+  function secondKeyDown() {
+    if (e.key === "Enter") {
+      submitRef.current.focus();
+    }
+  }
+
+  function submitKeyDown() {
+    if (e.key === "Enter") {
+      alert("Form Submitted!");
+    }
+  }
+
+  useEffect(() => {
+    firstNameRef.current.focus();
+  }, []);
 
   return (
     <div>
@@ -13,15 +36,18 @@ export default function Ref() {
           placeholder="Enter First Name"
           className="border border-slate-400 shadow-md p-2 w-80"
           ref={firstNameRef}
+          onKeyDown={firstKeyDown}
         />
         <input
           type="text"
           placeholder="Enter Second Name"
           className="border border-slate-400 shadow-md p-2 w-80"
           ref={secondNameRef}
+          onKeyDown={secondKeyDown}
         />
         <button
           ref={submitRef}
+          onKeyDown={submitKeyDown}
           className="border border-green-400 text-white p-1.5 bg-green-400 rounded-md px-4"
         >
           Submit
