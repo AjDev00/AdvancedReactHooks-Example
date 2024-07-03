e6c0a8aaeedaf30829a7ce22b92d12a0c69a168d
oluwatobi <oluwatobiajayi001@gmail.com>
2024-07-03 12:09:51 +0100

    modified:   src/App.jsx 	modified:   src/components/ForwardRef.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,17 +25,46 @@ function App() {
       alert("Form Submitted!");
     }
   }
+
+  useEffect(() => {
+    firstNameRef.current.focus();
+  }, []);
+
   return (
     <div className="App">
       <Ref />
-      <ForwardRef
+      <div className="pt-10 justify-center items-center flex flex-col gap-6">
+        <ForwardRef
+          type="text"
+          placeholder="Enter First Name"
+          // className="border border-slate-400 shadow-md p-2 w-80"
+          ref={firstNameRef}
+          onKeyDown={firstKeyDown}
+        />
+        <ForwardRef
+          type="text"
+          placeholder="Enter Second Name"
+          // className="border border-slate-400 shadow-md p-2 w-80"
+          ref={secondNameRef}
+          onKeyDown={secondKeyDown}
+        />
+        <button
+          ref={submitRef}
+          onKeyDown={submitKeyDown}
+          //   onClick={handleSubmit}
+          className="border border-green-400 text-white p-1.5 bg-green-400 rounded-md px-4"
+        >
+          Submit
+        </button>
+      </div>
+      {/* <ForwardRef
         firstNameRef={firstNameRef}
         secondNameRef={secondNameRef}
         submitRef={submitRef}
         firstKeyDown={firstKeyDown}
         secondKeyDown={secondKeyDown}
         submitKeyDown={submitKeyDown}
-      />
+      /> */}
     </div>
   );
 }
--- src/components/ForwardRef.jsx
+++ src/components/ForwardRef.jsx
@@ -1,37 +1,55 @@
-export default function ForwardRef({
-  firstNameRef,
-  secondNameRef,
-  submitRef,
-  firstKeyDown,
-  secondKeyDown,
-  submitKeyDown,
-}) {
+// export default function ForwardRef({
+//   firstNameRef,
+//   secondNameRef,
+//   submitRef,
+//   firstKeyDown,
+//   secondKeyDown,
+//   submitKeyDown,
+// }) {
+//   return (
+//     <div>
+//       <div className="pt-10 justify-center items-center flex flex-col gap-6">
+//         <input
+//           type="text"
+//           placeholder="Enter First Name"
+//           className="border border-slate-400 shadow-md p-2 w-80"
+//           ref={firstNameRef}
+//           onKeyDown={firstKeyDown}
+//         />
+//         <input
+//           type="text"
+//           placeholder="Enter Second Name"
+//           className="border border-slate-400 shadow-md p-2 w-80"
+//           ref={secondNameRef}
+//           onKeyDown={secondKeyDown}
+//         />
+//         <button
+//           ref={submitRef}
+//           onKeyDown={submitKeyDown}
+//           //   onClick={handleSubmit}
+//           className="border border-green-400 text-white p-1.5 bg-green-400 rounded-md px-4"
+//         >
+//           Submit
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+import React from "react";
+function ForwardRef({ type, placeholder, onKeyDown }, ref) {
   return (
     <div>
-      <div className="pt-10 justify-center items-center flex flex-col gap-6">
-        <input
-          type="text"
-          placeholder="Enter First Name"
-          className="border border-slate-400 shadow-md p-2 w-80"
-          ref={firstNameRef}
-          onKeyDown={firstKeyDown}
-        />
-        <input
-          type="text"
-          placeholder="Enter Second Name"
-          className="border border-slate-400 shadow-md p-2 w-80"
-          ref={secondNameRef}
-          onKeyDown={secondKeyDown}
-        />
-        <button
-          ref={submitRef}
-          onKeyDown={submitKeyDown}
-          //   onClick={handleSubmit}
-          className="border border-green-400 text-white p-1.5 bg-green-400 rounded-md px-4"
-        >
-          Submit
-        </button>
-      </div>
+      <input
+        className="border border-slate-400 shadow-md p-2 w-80"
+        type={type}
+        placeholder={placeholder}
+        onKeyDown={onKeyDown}
+        ref={ref}
+      />
     </div>
   );
 }
+
+const forwardedRef = React.forwardRef(ForwardRef);
+
+export default forwardedRef;
