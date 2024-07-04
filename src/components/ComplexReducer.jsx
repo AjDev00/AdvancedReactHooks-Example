c956f9e4d50d9245216d0ac8d0720d85872cfe12
oluwatobi <oluwatobiajayi001@gmail.com>
2024-07-04 14:53:41 +0100

    modified:   src/components/ComplexReducer.jsx

--- src/components/ComplexReducer.jsx
+++ src/components/ComplexReducer.jsx
@@ -5,20 +5,34 @@ export default function ComplexReducer() {
   //initialState is an object that holds the firstCounter variable of the counter.
   const initialState = {
     firstCounter: 0,
+    secondCounter: 10,
   };
 
   //the reducer function. the type is the type of operation that wants to be performed.
   function reducer(state, action) {
     switch (action.type) {
       case "increment":
-        return { firstCounter: state.firstCounter + 1 };
+        return { ...state, firstCounter: state.firstCounter + 1 };
 
       case "decrement":
         //decrement if firstCounter value can be deducted by one, if not return the initialState value.
         return {
+          ...state,
           firstCounter: state.firstCounter
             ? state.firstCounter - 1
             : state.firstCounter,
+        };
+
+      case "increment2":
+        return { ...state, secondCounter: state.secondCounter + action.value };
+
+      case "decrement2":
+        //decrement if firstCounter value can be deducted by one, if not return the initialState value.
+        return {
+          ...state,
+          secondCounter: state.secondCounter
+            ? state.secondCounter - action.value
+            : state.secondCounter,
         };
 
       case "reset":
@@ -37,23 +51,47 @@ export default function ComplexReducer() {
     <div>
       <div className="pt-20">
         <div className="flex flex-col justify-center items-center">
-          <h3 className="font-semibold underline">useReducer</h3>
-          <div>Count = {count.firstCounter}</div>
+          <h3 className="font-semibold underline">Complex useReducer</h3>
+          <div>First Counter = {count.firstCounter}</div>
+          <div>Second Counter = {count.secondCounter}</div>
         </div>
         <br />
+
+        {/* //first counter. */}
         <div className="flex flex-row gap-4 justify-center items-center">
           <button
             className="border border-transparent shadow-md p-2 rounded-md bg-green-400 text-white"
             onClick={() => dispatch({ type: "increment" })}
           >
-            Increment
+            Increment First Counter
           </button>
           <button
             className="border border-transparent shadow-md p-2 rounded-md bg-green-400 text-white"
             onClick={() => dispatch({ type: "decrement" })}
           >
-            Decrement
+            Decrement First Counter
           </button>
+        </div>
+        <br />
+
+        {/* //second counter. */}
+        <div className="flex flex-row gap-4 justify-center items-center">
+          <button
+            className="border border-transparent shadow-md p-2 rounded-md bg-green-400 text-white"
+            onClick={() => dispatch({ type: "increment2", value: 5 })}
+          >
+            Increment Second Counter
+          </button>
+          <button
+            className="border border-transparent shadow-md p-2 rounded-md bg-green-400 text-white"
+            onClick={() => dispatch({ type: "decrement2", value: 5 })}
+          >
+            Decrement Second Counter
+          </button>
+        </div>
+        <br />
+
+        <div className="flex flex-col justify-center items-center">
           <button
             className="border border-transparent shadow-md p-2 rounded-md bg-red-400 text-white"
             onClick={() => dispatch({ type: "reset" })}
